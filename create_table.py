68550fc490e419251902b82d8ccf9467d29d517d
bhavinv89 <51201786+bhavinv89@users.noreply.github.com>
2023-09-12 15:51:18 -0400

    Delete create_table.py

--- create_table.py
+++
@@ -1,9 +0,0 @@
-# create_tables.py
-from app import app, db  # Import the app instance
-
-# Import your models if necessary
-from models import Playlist, Song, PlaylistSong
-
-# Create the database tables using the db.create_all() function
-with app.app_context():
-    db.create_all()
